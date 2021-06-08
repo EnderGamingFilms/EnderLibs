4e889c27218ecc7f46d4bbe8e25baac60b65edb3
juicebin <juicebinned@gmail.com>
2021-06-08 18:17:57 -0400

    Update SubCommand.java

--- src/main/java/me/endergaming/enderlibs/command/SubCommand.java
+++ src/main/java/me/endergaming/enderlibs/command/SubCommand.java
@@ -29,17 +29,12 @@ public abstract class SubCommand extends BaseCommand {
         super(command, usage);
         this.plugin = plugin;
         this.hasCommandArgs = hasCommandArgs;
-        this.permission = String.format("%s.command.%s", plugin.getName(), command);
-
-        Permission bukkitPerm = new Permission(this.permission);
-        bukkitPerm.setDefault(PermissionDefault.OP);
-        Bukkit.getPluginManager().addPermission(bukkitPerm);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         // Do permission check
-        if (!sender.hasPermission(permission)) {
+        if (permission != null && !sender.hasPermission(permission)) {
             MessageUtils.send(sender, INVALID_PERMISSION);
             return true;
         }
@@ -111,5 +106,9 @@ public abstract class SubCommand extends BaseCommand {
 
     public void setPermission(String permission) {
         this.permission = permission;
+
+        Permission bukkitPerm = new Permission(this.permission);
+        bukkitPerm.setDefault(PermissionDefault.OP);
+        Bukkit.getPluginManager().addPermission(bukkitPerm);
     }
 }
